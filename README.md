90c6f0d8fa6236c0bd908317d8585ff5cecc416f
Yoshihiro OKUMURA <orrisroot@gmail.com>
2021-08-16 19:05:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# MediaWiki Extension AddKeywordsMetaTag
+
+This extension generates `keywords` meta tag header from MediaWiki:Keywords page and `<keywords content=".."/>' tags.
+
+## Install
+
+To install this extension, add the following to LocalSettings.php.
+
+```PHP
+wfLoadExtension("AddKeywordsMetaTag");
+```
+
+## Usage
+
+#### global keywords
+
+Edit `MediaWiki:Keywords` page.
+
+#### page specific keywords
+
+```MediaWiki
+<keywords content="foo, bar" />
+```
+
+## License
+
+This software is licensed under the [MIT](COPYING).
+
+## Authors
+
+- [Yoshihiro Okumura](https://github.com/orrisroot)
+
+## Usage examples
+
+- https://bsd.neuroinf.jp/ : Brain Science Dictionary project in Japanese.
---
+++ includes/Hooks.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MediaWiki\Extension\AddKeywordsMetaTag;
+
+class Hooks
+{
+    /**
+     * register tag renderer callbacks.
+     */
+    public static function onParserFirstCallInit(\Parser $parser)
+    {
+        $parser->setHook('keywords', [self::class, 'renderKeywordsTag']);
+    }
+
+    /**
+     * render <keywords> tag.
+     *
+     * @param string $input
+     *
+     * @return string
+     */
+    public static function renderKeywordsTag($input, array $params, \Parser $parser, \PPFrame $frame)
+    {
+        if (!isset($params['content'])) {
+            return '<div class="errorbox">Error: &lt;keywords&gt; tag must contain a &quot;content&quot; attribute.</div>';
+        }
+
+        return '<!-- META_KEYWORDS '.base64_encode($params['content']).' -->';
+    }
+
+    /**
+     * on output page before html.
+     *
+     * @param string &$text
+     */
+    public static function onOutputPageBeforeHTML(\OutputPage $out, &$text)
+    {
+        global $action;
+        $keywords = [];
+        if (in_array($action, ['edit', 'history', 'delete', 'watch'])) {
+            return;
+        }
+        // get keywords from MediaWiki:Keywords page.
+        $title = \Title::MakeTitle(NS_MEDIAWIKI, 'Keywords');
+        $article = new \Article($title);
+        $revision = $article->getRevision();
+        $content = $revision->getContent(\Revision::RAW);
+        foreach (explode(',', \ContentHandler::getContentText($content)) as $keyword) {
+            $keyword = trim($keyword);
+            if ('' !== $keyword) {
+                $keywords[] = $keyword;
+            }
+        }
+        // get keywords from rendered <keywords> tags.
+        if (preg_match_all('/<!-- META_KEYWORDS ([0-9a-zA-Z\\+\\/]+=*) -->/m', $text, $matches)) {
+            $data = $matches[1];
+            // Merge keyword data into OutputPage as meta tags
+            foreach ($data as $item) {
+                $content = @base64_decode($item);
+                foreach (explode(',', $content) as $keyword) {
+                    $keyword = trim($keyword);
+                    if ('' !== $keyword) {
+                        $keywords[] = $keyword;
+                    }
+                }
+            }
+        }
+        // add keywords meta tag.
+        $keywords = array_unique($keywords);
+        if (!empty($keywords)) {
+            $out->addMeta('keywords', implode(', ', $keywords));
+        }
+    }
+}
